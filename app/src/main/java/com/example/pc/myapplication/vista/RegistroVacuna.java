81470cdf85c1419135e3e459c5dfd5aac6eba067
Ow0287 <owrivero@misena.edu.co>
2017-07-12 19:11:34 -0500

    change database

--- app/src/main/java/com/example/pc/myapplication/vista/RegistroVacuna.java
+++ app/src/main/java/com/example/pc/myapplication/vista/RegistroVacuna.java
@@ -78,6 +78,9 @@ fecha.setText(fechaAct);
 
                 }
                 adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,vacu);
+                listaVacun.setAdapter(adapter);
+            }else {
+                Toast.makeText(this, "No hay Vacunas Registradas", Toast.LENGTH_SHORT).show();
             }
 
         }
